4711811d1571ce8883f6dc3ae0e3dca11809ec53
HAKIT1994 <hakitblack@gmail.com>
2022-07-15 22:09:26 +0800

    Basic

--- hwh-web/src/App.js
+++ hwh-web/src/App.js
@@ -2,24 +2,19 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
-import Profile from './components/pages/Profile'
-import Other from './components/pages/Other'
 import Footer from './components/Footer';
-
 import {
-  Router,
   Routes,
   Route,
 } from "react-router-dom";
+
 
 function App() {
   return (
     <>
     <Navbar/>
       <Routes>
-        <Route exact path='/' element={<Home/>} />
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/other' element={<Other/>}/>
+        <Route path='*' element={<Home/>} />
       </Routes>
     <Footer />
     </>
